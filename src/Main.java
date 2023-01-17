2dc1583631a05cc20703ba6a13c05098a9e10905
Jesus Arellano Ambrocio <aajesus@live.com.mx>
2023-01-17 15:05:52 -0600

    First commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        Coche miCoche = new Coche();
+        miCoche.aumentarPuertas();
+        System.out.println(miCoche.puertas);
+
+    }
+
+}
+
+ class Coche {
+    int puertas=2;
+
+    public void aumentarPuertas(){
+        puertas++;
+
+    }
+
+}
